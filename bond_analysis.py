326f8d2905798beebe03cbc7ae6e161099887080
PascalSalzbrenner <pts28@cam.ac.uk>
2021-04-12 21:01:29 +0200

    Implement bond_length and bond_population tasks

--- bond_analysis.py
+++ bond_analysis.py
@@ -13,7 +13,7 @@
 # bond length analysis: Takes the indices (bonds ranked by length from shortest to longest) of an arbitrary number of bonds and tracks their
 #                       length(s) as a function of pressure
 # bond population analysis: Likewise, but for the population. Only works from a .castep file
-# bonds: simply prints out data about the bonds in the bonds.dat file
+# bonds: simply prints out data about the bonds - name, length, (if read from a .castep file) populations - in the bonds.dat file
 
 # if one of the latter two tasks is selected, the code reads all relevant files in the directory, and the pressure is determined from them
 
@@ -61,7 +61,7 @@ def get_bonds_from_castep(filename):
     return (bonds, pressure)
 
 # get necessary input
-task = input("Which task would you like to run? (PDF, RDF, bond_length, bond_population - see the code header for descriptions) ").lower()
+task = input("Which task would you like to run? (PDF, RDF, bond_length, bond_population, bonds - see the code header for descriptions) ").lower()
 
 if task == "rdf" or task == "pdf" or task == "bonds":
     # different input files are possible
@@ -102,14 +102,118 @@ elif task == "rdf":
 
 elif task == "bond_length" or task == "bond_population":
 
-    # for these two tasks, loop over all files of the right type in the directory
-    # determine all those files
+    indices = "What bonds (indexed from the shortest=1) do you want to take into account?\nPass a whitespace-separated list of an arbitrary number.\na-b will lead to an average of all bonds in the range [a,b]. ".split()
 
-    # set up container
+    # for these two tasks, loop over all files of the right type in the directory
+
+    # determine all those files
     analysis_files = []
     for item in os.listdir():
         if "input_file_type" in item:
             analysis_files.append(item)
+
+    # determine which element of the data we need to read (1 for length, 2 for population)
+    if task == "bond_length":
+        data_index = 1
+    else:
+        data_index = 2
+
+    # data container
+    data_list=[]
+
+    for file in analysis_files:
+        if input_file_type == "castep":
+            bonds, pressure = get_bonds_from_castep(file)
+            pressure_units = "GPa"
+            length_units = " [A]"
+
+        else:
+            structure = Structure(file)
+            structure.get_bonds()
+            bonds = structure.bonds
+            pressure = structure.pressure
+            pressure_units = structure.pressure_units
+            length_units = structure.length_units
+
+        # initialise string for the line
+        write_str = "{}".format(pressure)
+
+        # iterate over indices
+        for index in indices:
+
+            # check if we are averaging over a number of bonds
+            if "-" in index:
+                start_end_indices = index.split("-")
+                start_index = int(start_end_indices[0])
+                end_index = int(start_end_indices[1])
+                num_points = end_index - start_index + 1
+                average = 0
+
+                for i in range(start_index-1, end_index):
+                    average += bonds[i][data_index]
+
+                write_str += " {}".format(average/num_points)
+            else:
+                # only a single point
+                write_str += " {}".format(bonds[index-1][data_index])
+
+    # determine the y-units depending on the task
+    if task == "bond_length":
+        y_units = " [{}]".format()
+    else:
+        y_units = ""
+
+    # sort data in ascending order of pressure
+    data_list.sort()
+
+    # write data
+    datafile = open("{}.dat".format(task), "w")
+    datafile.write("# Pressure [{}]; {}{}\n".format(pressure_units, task, y_units))
+    datafile.write("\n".join(data_list))
+    datafile.close()
+
+
+    # plotting stuff
+    # redefine gnuplot linetypes with nice colours
+    plotfile.write("set linetype 1 lc rgb '#DC143C'\n")
+    plotfile.write("set linetype 2 lc rgb '#DC143C'\n")
+    plotfile.write("set linetype 3 lc rgb '#D95F02'\n")
+    plotfile.write("set linetype 4 lc rgb '#D95F02'\n")
+    plotfile.write("set linetype 5 lc rgb '#E6AB02'\n")
+    plotfile.write("set linetype 6 lc rgb '#E6AB02'\n")
+    plotfile.write("set linetype 7 lc rgb '#66A61E'\n")
+    plotfile.write("set linetype 8 lc rgb '#66A61E'\n")
+    plotfile.write("set linetype 9 lc rgb '#8000C4'\n")
+    plotfile.write("set linetype 10 lc rgb '#8000C4'\n")
+    plotfile.write("set linetype 11 lc rgb '#7570B3'\n")
+    plotfile.write("set linetype 12 lc rgb '#7570B3'\n")
+    plotfile.write("set linetype 13 lc rgb '#E7298A'\n")
+    plotfile.write("set linetype 14 lc rgb '#E7298A'\n")
+    plotfile.write("set linetype 15 lc rgb '#1E90FF'\n")
+    plotfile.write("set linetype 16 lc rgb '#1E90FF'\n")
+    plotfile.write("set linetype 17 lc rgb '#1B9E77'\n")
+    plotfile.write("set linetype 18 lc rgb '#1B9E77'\n")
+    plotfile.write("set linetype 19 lc rgb '#B8860B'\n")
+    plotfile.write("set linetype 20 lc rgb '#B8860B'\n")
+    plotfile.write("set linetype 21 lc rgb '#20C2C2'\n")
+    plotfile.write("set linetype 22 lc rgb '#20C2C2'\n")
+    plotfile.write("set linetype cycle 22\n")
+
+    plotfile.write("set xlabel '{} [{}]'\n".format(pressure, pressure_units))
+
+    # format the task string so as to use it for the y-axis label
+    task_strings = task.split("_")
+    task_string_nice = "{} {}".format(task_strings[0].capitalize(), task_strings[1].capitalize())
+    plotfile.write("set ylabel '{}{}'\n".format(task_string_nice, y_units))
+
+    # compile string for plotting the data
+    plot_string = "plot"
+
+    for i in range(len(indices)):
+        plot_string.append(" {0}.dat u 1:{1} w points pt 7 ps 1.5 title 'Bond(s) {2}', {0}.dat u 1:{1} smooth mcsplines notitle".format(task, i+2, indices[i]))
+
+    plotfile.write(plot_string)
+
 
 elif task == "bonds":
 
