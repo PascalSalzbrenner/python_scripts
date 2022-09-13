7bee12984d633e053d76c5eb7465bd4d2cf3d28c
PascalSalzbrenner <pts28@cam.ac.uk>
2022-09-13 11:13:05 +0100

    Rename output file to include energy window

--- ramble_average.py
+++ ramble_average.py
@@ -61,9 +61,9 @@ average_enthalpy = enthalpies.mean()
 average_total_energy = total_energies.mean()
 
 # write out data
-with open("{}_averages.txt".format(fileroot), "w") as outfile:
+with open("{}_{}_{}_ps_averages.txt".format(fileroot, initial_step, final_step), "w") as outfile:
 
-    outfile.write("# Average values along the MD trajectory, calculated from {} to {} ps\n\n".format(initial_step, final_step))
+    outfile.write("# Average values along the MD trajectory, calculated from {:d} to {:f.3} ps\n\n".format(initial_step, final_step))
     outfile.write("Volume: {} A**3/atom\n".format(average_volume))
     outfile.write("Temperature: {} K\n".format(average_temperature))
     outfile.write("Pressure: {} GPa\n".format(average_pressure))
