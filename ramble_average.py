3f0358a6582e771c5fc0295ff2ee0f74586b6474
PascalSalzbrenner <pts28@cam.ac.uk>
2022-09-21 16:19:28 +0100

    Update names of variables and some comments to better reflect ramble

--- ramble_average.py
+++ ramble_average.py
@@ -1,5 +1,5 @@
 # script to calculate the average of the quantities calculated during a ramble MD run and printed in the .track file
-# written by Pascal Salzbrenner, pts28
+# written by Pascal Salzbrenner, pts28@cam.ac.uk
 
 import sys
 import numpy as np
@@ -22,8 +22,8 @@ trackfile = open("{}.track".format(fileroot), "r")
 volumes = [] # [A**3/atom], second column
 temperatures = [] # [K], third column
 pressures = [] # [GPa], fifth column
-enthalpies = [] # [eV/atom], sixth column
-total_energies = [] # (enthalpy + kinetic energy) [eV/atom], eighth column
+internal_energies_plus_PV = [] # [eV/atom], sixth column (column 8 minus the KE)
+total_energies = [] # [eV/atom], eighth column (total energy when p=0, enthalpy otherwise)
 
 # read data
 for line in trackfile:
@@ -34,7 +34,7 @@ for line in trackfile:
         volumes.append(float(data[1]))
         temperatures.append(float(data[2]))
         pressures.append(float(data[4]))
-        enthalpies.append(float(data[5]))
+        internal_energies_plus_PV.append(float(data[5]))
         total_energies.append(float(data[7]))
     elif final_step and float(data[0]) > final_step:
         break
@@ -51,20 +51,20 @@ if not final_step:
 volumes = np.array(volumes)
 temperatures = np.array(temperatures)
 pressures = np.array(pressures)
-enthalpies = np.array(enthalpies)
+internal_energies_plus_PV = np.array(internal_energies_plus_PV)
 total_energies = np.array(total_energies)
 
 average_volume = volumes.mean()
 average_temperature = temperatures.mean()
 average_pressure = pressures.mean()
-average_enthalpy = enthalpies.mean()
+average_internal_energy_plus_PV = internal_energies_plus_PV.mean()
 average_total_energy = total_energies.mean()
 
 # calculate standard deviations
 volume_std = volumes.std()
 temperature_std = temperatures.std()
 pressure_std = pressures.std()
-enthalpy_std = enthalpies.std()
+internal_energy_plus_PV_std = internal_energies_plus_PV.std()
 total_energy_std = total_energies.std()
 
 # write out data
@@ -74,5 +74,5 @@ with open("{}_{:d}_{:d}_ps_averages.txt".format(fileroot, int(initial_step), int
     outfile.write("Volume: {} A**3/atom; Standard deviation {} A**3/atom\n".format(average_volume, volume_std))
     outfile.write("Temperature: {} K; Standard deviation {} K\n".format(average_temperature, temperature_std))
     outfile.write("Pressure: {} GPa; Standard deviation {} GPa\n".format(average_pressure,  pressure_std))
-    outfile.write("U_plus_PV: {} eV/atom; Standard deviation {} eV/atom\n".format(average_enthalpy, enthalpy_std))
+    outfile.write("U_plus_PV: {} eV/atom; Standard deviation {} eV/atom\n".format(average_internal_energy_plus_PV, internal_energy_plus_PV))
     outfile.write("Total_Energy_Enthalpy: {} eV/atom; Standard deviation {} eV/atom\n".format(average_total_energy, total_energy_std))
