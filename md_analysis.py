bee9ef0c7ef06a64efef5116807d7f7c0a5fcea3
PascalSalzbrenner <pts28@cam.ac.uk>
2022-09-28 13:29:47 +0100

    Begin md_analysis script

---
+++ md_analysis.py
@@ -0,0 +1,4 @@
+# a script to analyse the output of an MD run in the form of a .xyz(e) trajectory
+# basically a wrapper to some ASE functionalities
+
+# written by Pascal Salzbrenner, pts28@cam.ac.uk
