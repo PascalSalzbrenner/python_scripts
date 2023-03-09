249b06f22d71d7cf07b4833823514c3c04f633cd
Pascal Salzbrenner <pts28@cam.ac.uk>
2023-03-09 14:08:57 +0000

    Remove use of depracted input variable

--- debye_temperature_caesar.py
+++ debye_temperature_caesar.py
@@ -49,7 +49,6 @@ debye_temperature = conversion_factor * debye_frequency
 
 # write output
 with open("debye_temperature.dat", "w") as outfile:
-	outfile.write("The Debye frequency of {} is {} Hartree.\n".format(seed, debye_frequency))
 	outfile.write("The Debye temperature, therefore, is {} K.".format(debye_temperature))
 
 
