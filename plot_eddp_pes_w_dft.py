8e9f3a256d7a1c01f9a3fd50c558dd90fa9c404f
Pascal Salzbrenner <pts28@cam.ac.uk>
2023-05-03 16:57:38 +0100

    Add script to plot the output of a ddp-pes_all run with DFT data at half opacity

---
+++ plot_eddp_pes_w_dft.py
@@ -0,0 +1,8 @@
+# script to plot the output of a ddp-pes_all run, with the DFT at half opacity
+# written by Pascal Salzbrenner, pts28@cam.ac.uk
+
+import sys
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
