6c88f39c725690d84cbc950fde0769304e107c17
Pascal Salzbrenner <pts28@cam.ac.uk>
2024-12-16 16:56:56 +0000

    Fix bug

--- birch_murnaghan_eos_fit.py
+++ birch_murnaghan_eos_fit.py
@@ -8,8 +8,6 @@ import math
 import numpy as np
 from scipy.optimize import curve_fit
 
-from birch_murnaghan_eos_fit_thermal_expansion import prefactor
-
 
 # define Birch-Murnaghan equation of state for E(V) - V is input, the other variables are
 # parameters to be fit
